26a663c38599c3f2a42ea32423143943dfc31a05
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2017-04-19 18:37:13 +0200

    Update piwik tracking code

--- static/js/piwik.js
+++ static/js/piwik.js
@@ -1,13 +1,12 @@
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDomains", ["*.blog.erad.fr","*.op.erad.fr"]]);
-_paq.push(["enableCrossDomainLinking"]);
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-    var u="//stats.erad.fr/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  var u="//stats.erad.fr/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
