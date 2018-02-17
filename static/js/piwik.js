aebfd0152d189e091ce06f4945edb0ca5c8d276c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2018-02-17 17:11:44 +0100

    new stats

--- static/js/piwik.js
+++ static/js/piwik.js
@@ -4,7 +4,7 @@ _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//stats.erad.fr/";
+  var u="//stats.idrissi.eu/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
