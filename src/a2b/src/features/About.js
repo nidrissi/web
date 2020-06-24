c2c10d41f376b281850e1f762f1c094f91b968b3
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 17:09:53 +0200

    expand about

--- src/a2b/src/features/About.js
+++ src/a2b/src/features/About.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function About() {
   return (
-    <p>About!</p>
+    <div>
+      <p>
+        This tool is designed to produce a biblatex entry from an arXiv id.
+        It is in a very preliminary state and probably full of bugs.
+        Caveat emptor!
+      </p>
+      <p>
+        It was written by <a href="https://idrissi.eu">Najib Idrissi</a>.
+        The source is <a href="https://github.com/nidrissi/arxiv2bib">available on GitHub</a>.
+      </p>
+    </div>
   )
 }
