3ee9a0d05c2f840335820a1f632b84e57b56eae7
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 09:44:07 +0200

    navbar

--- src/a2b/src/App.js
+++ src/a2b/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 import Footer from './features/Footer';
+import MyNavbar from './features/MyNavbar';
 
 function App() {
   return (
     <Container>
+      <MyNavbar />
       <p>Hello!</p>
       <Footer />
     </Container>
