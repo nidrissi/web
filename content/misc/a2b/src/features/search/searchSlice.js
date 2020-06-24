8ddeb36ba4f0c290e048fe31a9ed8b78f9484130
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 10:06:19 +0200

    create searchSlice

---
+++ content/misc/a2b/src/features/search/searchSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const searchSlice = createSlice({
+  name: 'search',
+  initialState : {},
+  reducers : {}
+});
+
+export default searchSlice.reducer;
