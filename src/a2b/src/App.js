616801b4017efc2d0c7929aec03f466cf0e981b5
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 09:51:51 +0200

    router

--- src/a2b/src/App.js
+++ src/a2b/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Footer from './features/Footer';
 import MyNavbar from './features/MyNavbar';
@@ -7,8 +8,20 @@ import MyNavbar from './features/MyNavbar';
 function App() {
   return (
     <Container>
-      <MyNavbar />
-      <p>Hello!</p>
+      <Router>
+        <MyNavbar />
+        <Switch>
+          <Route path="/" exact>
+            <p>Hello!</p>
+          </Route>
+          <Route path="/about">
+            <p>About?!</p>
+          </Route>
+          <Route path="*">
+            <p>404</p>
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </Container>
   );
