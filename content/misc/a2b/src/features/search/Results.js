a28c157aea17ffa005e62ae333d6306a43aaa02c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 11:21:47 +0200

    error & isloading

--- content/misc/a2b/src/features/search/Results.js
+++ content/misc/a2b/src/features/search/Results.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import Alert from 'react-bootstrap/Alert'
+import Alert from 'react-bootstrap/Alert';
+import Spinner from 'react-bootstrap/Spinner';
 
 import {
+  selectError,
+  selectIsLoading,
   selectResults,
 } from './resultsSlice';
 
 export default function Results() {
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
   const results = useSelector(selectResults);
 
-  return (
-    <div>
-      {
-        results.map(s =>
-          <Alert variant="dark" key={s}>
-            <pre className="m-0">
-              <code>
-                {s}
-              </code>
-            </pre>
-          </Alert>
-        )
-      }
-    </div>
-  );
+  if (error !== null) {
+    return (
+      <Alert variant="danger">
+        <strong>Error: </strong>
+        {error}
+      </Alert>
+    )
+  } else if (isLoading) {
+    return (
+      <Alert variant="info">
+        <Spinner animation="border" size="sm" />
+        {' '}
+        Loading...
+      </Alert>
+    )
+  } else {
+    return (
+      <div>
+        {
+          results.map(s =>
+            <Alert variant="dark" key={s}>
+              <pre className="m-0">
+                <code>
+                  {s}
+                </code>
+              </pre>
+            </Alert>
+          )
+        }
+      </div>
+    )
+  }
 }
--- content/misc/a2b/src/features/search/resultsSlice.js
+++ content/misc/a2b/src/features/search/resultsSlice.js
@@ -2,19 +2,25 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const resultsSlice = createSlice({
   name: 'results',
-  initialState : ['salut'],
+  initialState :{
+    error: null,
+    isLoading: false,
+    data: ['salut']
+  },
   reducers : {
     pushEntry : (state, action) => {
-      state.push(action.payload)
+      state.data.push(action.payload)
     },
     clean : state => {
-      state = []
+      state.data = []
     }
   }
 });
 
 export const { pushEntry, clean } = resultsSlice.actions;
 
-export const selectResults = state => state.results;
+export const selectResults = state => state.results.data;
+export const selectIsLoading = state => state.results.isLoading;
+export const selectError = state => state.results.error;
 
 export default resultsSlice.reducer;
