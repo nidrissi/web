e5ba68dce3d50b2170c8c79af5ceb2885cc09cb9
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 10:42:48 +0200

    add search button

--- content/misc/a2b/src/features/search/SearchForm.js
+++ content/misc/a2b/src/features/search/SearchForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
@@ -23,6 +24,16 @@ export default function SearchForm() {
             value={id}
             onChange={e => dispatch(setId(e.target.value))}
           />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row}>
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button
+            type="submit"
+            onClick={e => e.preventDefault()}
+          >
+            Search
+          </Button>
         </Col>
       </Form.Group>
     </Form>
