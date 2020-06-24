5fbc536d36943ac56ae7840e6ffeb92c8df8721c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 17:34:00 +0200

    link to issues

--- content/misc/a2b/src/features/About.js
+++ content/misc/a2b/src/features/About.js
@@ -6,11 +6,13 @@ export default function About() {
       <p>
         This tool is designed to produce a biblatex entry from an arXiv id.
         It is in a very preliminary state and probably full of bugs.
+        There are no tests and pretty much no error handling in the code.
         Caveat emptor!
       </p>
       <p>
         It was written by <a href="https://idrissi.eu">Najib Idrissi</a>.
         The source is <a href="https://github.com/nidrissi/arxiv2bib">available on GitHub</a>.
+        Feel free to <a href="https://github.com/nidrissi/arxiv2bib/issues">report issues there</a>.
       </p>
     </div>
   )
