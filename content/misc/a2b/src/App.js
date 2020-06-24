daaf20f9b06181623cbcc7309e416cea9f35ca08
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 09:40:31 +0200

    move footer to own component

--- content/misc/a2b/src/App.js
+++ content/misc/a2b/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-
 import Container from 'react-bootstrap/Container';
+
+import Footer from './features/Footer';
 
 function App() {
   return (
     <Container>
       <p>Hello!</p>
-      <footer className="text-center">
-        <hr />
-        <p>
-          arXiv2bib v{process.env.REACT_APP_VERSION} &bull; maintained by <a href="https://idrissi.eu">Najib Idrissi</a>
-        </p>
-      </footer>
+      <Footer />
     </Container>
   );
 }
