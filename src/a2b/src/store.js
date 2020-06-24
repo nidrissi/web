e69b040814a553390485476519c73fda0fdda5cc
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 11:00:41 +0200

    very basic results display

--- src/a2b/src/store.js
+++ src/a2b/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchFormReducer from './features/search/searchFormSlice';
+import resultsReducer from './features/search/resultsSlice';
 
 export default configureStore({
   reducer: {
-    searchForm: searchFormReducer
+    searchForm: searchFormReducer,
+    results: resultsReducer,
   },
 });
