22404a45a219cb6dcbe832be0dcd1d95792a6528
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-24 09:37:46 +0200

    footer: version + link

--- content/misc/a2b/src/App.js
+++ content/misc/a2b/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+
 function App() {
   return (
-    <p>Hello!</p>
+    <Container>
+      <p>Hello!</p>
+      <footer className="text-center">
+        <hr />
+        <p>
+          arXiv2bib v{process.env.REACT_APP_VERSION} &bull; maintained by <a href="https://idrissi.eu">Najib Idrissi</a>
+        </p>
+      </footer>
+    </Container>
   );
 }
 
