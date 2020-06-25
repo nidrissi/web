365be5481c402c5e7529080fed4f0f026015366e
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-25 15:22:11 +0200

    404

--- content/misc/a2b/src/features/Err404.js
+++ content/misc/a2b/src/features/Err404.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export default function Err404() {
   return (
-    <p>404...</p>
+    <Jumbotron>
+      <h1 className="text-center">404 not found</h1>
+    </Jumbotron>
   );
 }
