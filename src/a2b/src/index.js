65356f39499dd961499d1cfe4c10247019b224e2
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-25 17:13:47 +0200

    move GA + router

--- src/a2b/src/index.js
+++ src/a2b/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+
+ReactGA.initialize('UA-170792065-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
