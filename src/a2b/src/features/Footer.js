f7476b41dbe66c328725d5769968019527928334
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-25 09:21:51 +0200

    fancy version number

--- src/a2b/src/features/Footer.js
+++ src/a2b/src/features/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function Footer() {
+  const version = process.env.REACT_APP_VERSION.replace('-alpha', 'ɑ').replace('-beta', 'β')
+
   return (
     <footer className="text-center">
       <hr />
       <p>
-        arXiv2bib v{process.env.REACT_APP_VERSION} &bull; maintained by <a href="https://idrissi.eu">Najib Idrissi</a>
+        arXiv2bib v{version} &bull; maintained by <a href="https://idrissi.eu">Najib Idrissi</a>
       </p>
     </footer>
 
