4605fe1458bf6ee752a2857efe2eb67cd3403d78
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-25 09:48:46 +0200

    add pdf link

--- src/a2b/src/features/Results/EntryList.js
+++ src/a2b/src/features/Results/EntryList.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert';
 
-function Entry({ authors, id, title, year }) {
+function Entry({ entry }) {
   // deal with authors
   // transforms something like ['Jane Doe', 'John Dew'] into "Doe, Jane and Dew, John"
   // and key = "DoeDew"
-  const splitAuthors = authors.map(a => a.split(' '));
+  const splitAuthors = entry.authors.map(a => a.split(' '));
   const key = splitAuthors.map(l => l[l.length - 1]).join('');
   const authorList = splitAuthors.map(l => l[l.length - 1] + ', ' + l.slice(0, -1).join(' ')).join(' and ');
 
-  const formattedEntry = `@Misc{${key}${year},
-  date       = {${year}},
-  authors    = {${authorList}},
-  title      = {${title}},
-  eprint     = {${id}},
-  eprinttype = {arXiv},
-  pubstate   = {prepublished},
-  file       = {${key[0]}/${key}${year}.pdf}
-}`;
+  const fileLink = (
+    <a href={entry.pdfLink}>
+      {key[0]}/{key}{entry.year}.pdf
+    </a>
+  );
 
+  // very ugly ☹
   return (
     <Alert variant="dark">
       <pre className="m-0">
-        {formattedEntry}
+        {`@Misc{${key}${entry.year},
+  date       = {${entry.year}},
+  authors    = {${authorList}},
+  title      = {${entry.title}},
+  eprint     = {${entry.id}},
+  eprinttype = {arXiv},
+  pubstate   = {prepublished},
+  file       = {`}
+        {fileLink}
+        {`}
+}`}
       </pre>
     </Alert>
   )
@@ -32,10 +39,7 @@ export default function EntryList({ entries }) {
   const renderedEntries = entries.map(e =>
     <Entry
       key={e.id}
-      authors={e.authors}
-      id={e.id}
-      title={e.title}
-      year={e.year}
+      entry={e}
     />
   );
   return <div>{renderedEntries}</div>
