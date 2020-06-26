1d5b7c9f001f21c7825a634c466578114fafe1bf
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-26 13:32:32 +0200

    reorganize to avoid re-fetching every time

--- src/a2b/src/App.js
+++ src/a2b/src/App.js
@@ -1,15 +1,28 @@
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
-import Container from 'react-bootstrap/Container';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Router, Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
+import Container from 'react-bootstrap/Container';
+
+// my components
 import About from './features/About';
 import Err404 from './features/Err404';
 import Footer from './features/Footer';
 import MyNavbar from './features/MyNavbar';
 import Search from './features/Search';
 
+// state
+import {
+  selectIds,
+  selectAuthors,
+  selectTitles,
+} from './features/SearchForm/searchFormSlice';
+import { fetchEntries } from './features/Results/resultsSlice';
+
+// GA
+import ReactGA from 'react-ga';
 const history = createBrowserHistory()
 history.listen(location => {
   ReactGA.set({ page: location.pathname })
@@ -20,6 +33,19 @@ function App() {
   useEffect(() => {
     ReactGA.pageview(window.location.pathname)
   }, []);
+
+  // keep here to avoid re-fetching on route change
+  const dispatch = useDispatch();
+  const ids = useSelector(selectIds);
+  const authors = useSelector(selectAuthors);
+  const titles = useSelector(selectTitles);
+
+  useEffect(() => {
+    if (authors.length > 0 || ids.length > 0 || titles.length > 0) {
+      dispatch(fetchEntries({ authors, ids, titles }));
+    }
+  }, [dispatch, ids, authors, titles])
+
 
   return (
     <Container>
--- src/a2b/src/features/Results/index.js
+++ src/a2b/src/features/Results/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
@@ -8,14 +8,7 @@ import {
   selectError,
   selectIsLoading,
   selectEntries,
-  fetchEntries,
 } from './resultsSlice';
-
-import {
-  selectAuthors,
-  selectIds,
-  selectTitles,
-} from '../SearchForm/searchFormSlice';
 
 import EntryList from './EntryList';
 
@@ -46,21 +39,9 @@ function ErrorAlert({ error }) {
 }
 
 export default function Results() {
-  const dispatch = useDispatch();
-
   const error = useSelector(selectError);
   const isLoading = useSelector(selectIsLoading);
   const entries = useSelector(selectEntries);
-
-  const ids = useSelector(selectIds);
-  const authors = useSelector(selectAuthors);
-  const titles = useSelector(selectTitles);
-
-  useEffect(() => {
-    if (authors.length > 0 || ids.length > 0 || titles.length > 0) {
-      dispatch(fetchEntries({ authors, ids, titles }));
-    }
-  }, [dispatch, ids, authors, titles])
 
   return (
     <div>
