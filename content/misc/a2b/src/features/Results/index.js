6dfb6db465431a866da3be7ea729f7dcf64dbbb5
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-26 14:45:57 +0200

    nicer loading animation

--- content/misc/a2b/src/features/Results/index.js
+++ content/misc/a2b/src/features/Results/index.js
@@ -2,29 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Alert from 'react-bootstrap/Alert';
-import Spinner from 'react-bootstrap/Spinner';
 
 import {
   selectError,
-  selectIsLoading,
   selectEntries,
 } from './resultsSlice';
 
 import EntryList from './EntryList';
-
-function LoadingAlert({ isLoading = false }) {
-  if (isLoading) {
-    return (
-      <Alert variant="info">
-        <Spinner animation="border" size="sm" />
-        {' '}
-        Loading...
-      </Alert>
-    )
-  } else {
-    return null
-  }
-}
 
 function ErrorAlert({ error }) {
   if (error !== null) {
@@ -40,12 +24,10 @@ function ErrorAlert({ error }) {
 
 export default function Results() {
   const error = useSelector(selectError);
-  const isLoading = useSelector(selectIsLoading);
   const entries = useSelector(selectEntries);
 
   return (
     <div>
-      <LoadingAlert isLoading={isLoading} />
       <ErrorAlert error={error} />
       <EntryList entries={entries} />
     </div>
