1317353b4060360ac0e0f12da4306b5928f4641f
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-29 13:54:35 +0200

    dismissible error

--- content/misc/a2b/src/features/Results/resultsSlice.js
+++ content/misc/a2b/src/features/Results/resultsSlice.js
@@ -6,6 +6,7 @@ export const resultsSlice = createSlice({
   name: 'results',
   initialState: {
     error: null,
+    errorShown: false,
     isLoading: false,
     entries: []
   },
@@ -14,21 +15,28 @@ export const resultsSlice = createSlice({
     getEntriesSuccess: (state, action) => {
       state.isLoading = false;
       state.error = null;
+      state.errorShown = true;
       state.entries = action.payload;
     },
     getEntriesError: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
+      state.errorShown = false;
       state.entries = [];
     },
+    errorWasShown: state => {
+      state.errorShown = true;
+    }
   }
 });
 
 const { getEntriesStart, getEntriesSuccess, getEntriesError } = resultsSlice.actions;
+export const  { errorWasShown } = resultsSlice.actions;
 
 export const selectEntries = state => state.results.entries;
 export const selectIsLoading = state => state.results.isLoading;
 export const selectError = state => state.results.error;
+export const selectErrorShown = state => state.results.errorShown;
 
 export default resultsSlice.reducer;
 
