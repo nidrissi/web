12f8e790db12e0c7618532de3a1b1fba5c1bc5a4
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-07-02 19:00:43 +0200

    title

--- src/a2b/src/features/Search.js
+++ src/a2b/src/features/Search.js
@@ -6,7 +6,7 @@ import SearchForm from './SearchForm';
 export default function Search() {
   return (
     <div>
-      <h1>Search</h1>
+      <h1>Search arXiv</h1>
       <SearchForm />
       <Results />
     </div>
