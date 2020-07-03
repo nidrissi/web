852168d5b1fe1eee9597087d469756b94aedffdc
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-07-03 10:44:27 +0200

    bundle settings

--- src/a2b/src/features/Results/Entry.js
+++ src/a2b/src/features/Results/Entry.js
@@ -1,18 +1,11 @@
 import React, { useRef } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  selectIncludeAbstract,
-  selectIncludeFile,
-  selectFilePrefix,
-  selectIncludePrimaryCategory,
-  selectIncludeVersion,
-} from '../Settings/settingsSlice';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function formatEntry({ entry, includeAbstract, includeFile, filePrefix, includePrimaryCategory, includeVersion }) {
+function formatEntry({ entry, settings }) {
   // deal with authors
   // transforms something like ['Jane Doe', 'John Dew'] into "Doe, Jane and Dew, John"
   // and key = "DoeDew"
@@ -22,7 +15,7 @@ function formatEntry({ entry, includeAbstract, includeFile, filePrefix, includeP
 
   const fileLink = (
     <a href={entry.pdfLink}>
-      {filePrefix ? `${key[0]}/` : ''}{key}.pdf
+      {settings.filePrefix ? `${key[0]}/` : ''}{key}.pdf
     </a>
   );
 
@@ -33,14 +26,14 @@ function formatEntry({ entry, includeAbstract, includeFile, filePrefix, includeP
     title: entry.title,
     eprint: <a href={`https://arxiv.org/abs/${entry.id}`}>{entry.id}</a>,
     eprinttype: 'arXiv',
-    eprintclass: includePrimaryCategory ? entry.primaryCategory : null,
-    version: includeVersion ? entry.version : null,
+    eprintclass: settings.includePrimaryCategory ? entry.primaryCategory : null,
+    version: settings.includeVersion ? entry.version : null,
     pubstate: (entry.doi || entry.journaRef) ? null : 'prepublished',
     howpublished: entry.journalRef ? <abbr title="Consider converting this entry to @article or something more appropriate.">{entry.journalRef}</abbr> : null,
     doi: entry.doi,
-    file: includeFile ? fileLink : null,
+    file: settings.includeFile ? fileLink : null,
     comment: entry.comment,
-    abstract: includeAbstract ? entry.abstract : null,
+    abstract: settings.includeAbstract ? entry.abstract : null,
   };
   // delete all empty values
   Object.keys(pairing).forEach(k => !pairing[k] && delete pairing[k]);
@@ -49,7 +42,7 @@ function formatEntry({ entry, includeAbstract, includeFile, filePrefix, includeP
   const maxKeyLength = Math.max(...Object.keys(pairing).map(s => s.length));
 
   // pad the abstract
-  if (includeAbstract) {
+  if (settings.includeAbstract) {
     pairing.abstract = pairing.abstract.replace(/\n/g, "\n" + ' '.repeat(maxKeyLength + 6));
   }
 
@@ -79,11 +72,7 @@ export default function Entry({ entry }) {
     navigator.clipboard.writeText(preRef.current.innerText)
   };
 
-  const includeAbstract = useSelector(selectIncludeAbstract);
-  const includeFile = useSelector(selectIncludeFile);
-  const filePrefix = useSelector(selectFilePrefix);
-  const includePrimaryCategory = useSelector(selectIncludePrimaryCategory);
-  const includeVersion = useSelector(selectIncludeVersion);
+  const settings = useSelector(state => state.settings);
 
   return (
     <Card body bg="light" text="dark">
@@ -94,7 +83,7 @@ export default function Entry({ entry }) {
         ref={preRef}
         className="m-0"
       >
-        {formatEntry({ entry, includeAbstract, includeFile, filePrefix, includePrimaryCategory, includeVersion })}
+        {formatEntry({ entry, settings })}
       </pre>
     </Card>
   )
