a86c6b2cfac0ff341fa6d8b3df57e72b3bcdc27e
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-07-03 15:51:21 +0200

    add type to entries

--- src/a2b/src/features/Results/Entry.js
+++ src/a2b/src/features/Results/Entry.js
@@ -5,6 +5,9 @@ import { selectEntryById } from './resultsSlice';
 import Entry from '../Entry';
 
 export default function EntryById({ entryId }) {
-  const entry = useSelector(selectEntryById(entryId));
-  return <Entry entry={entry}/>
+  const entry = {
+    ...useSelector(selectEntryById(entryId)),
+    type: 'Misc'
+  };
+  return <Entry entry={entry} />
 }
