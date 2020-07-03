ff2456fd142ed8d4807fcf3f63e9c8a600e9cc3a
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-07-03 17:13:00 +0200

    add more DIY features

--- src/a2b/src/features/DIY.js
+++ src/a2b/src/features/DIY.js
@@ -7,13 +7,20 @@ import Row from 'react-bootstrap/Row';
 import Entry from './Entry';
 
 export default function DIY() {
-  const [authors, setAuthors] = useState([]);
   const [type, setType] = useState('Article');
-  
+  const [authors, setAuthors] = useState([]);
+  const [year, setYear] = useState(0);
+  const [journal, setJournal] = useState('');
+  const [id, setId] = useState('');
+  const [doi, setDoi] = useState('');
+
   const entry = {
     type,
     authors: authors.map(s => s.trim()),
-    year: 0
+    year: year || 0,
+    journal: (type === 'Article') ? journal : null,
+    id,
+    doi
   };
   return (
     <>
@@ -52,6 +59,72 @@ export default function DIY() {
               id='authors'
               value={authors.join('&')}
               onChange={e => setAuthors(e.target.value.split(/&/))}
+              placeholder='Author names separated by &'
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label
+            column sm={2}
+            htmlFor='year'
+          >
+            Year
+          </Form.Label>
+          <Col>
+            <Form.Control
+              id='year'
+              type='number'
+              value={year}
+              onChange={e => setYear(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group
+          as={Row}
+          className={type !== 'Article' ? 'd-none' : ''}
+        >
+          <Form.Label
+            column sm={2}
+            htmlFor='journal'
+          >
+            Journal
+          </Form.Label>
+          <Col>
+            <Form.Control
+              id='journal'
+              value={journal}
+              onChange={e => setJournal(e.target.value)}
+              disabled={type !== 'Article'}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label
+            column sm={2}
+            htmlFor='id'
+          >
+            ArXiv ID
+          </Form.Label>
+          <Col>
+            <Form.Control
+              id='id'
+              value={id}
+              onChange={e => setId(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label
+            column sm={2}
+            htmlFor='doi'
+          >
+            DOI
+          </Form.Label>
+          <Col>
+            <Form.Control
+              id='doi'
+              value={doi}
+              onChange={e => setDoi(e.target.value)}
             />
           </Col>
         </Form.Group>
