46504c2945afb354e8b82ba87168c55bd67e9665
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-08-18 12:41:59 +0200

    Add commit SHA

--- content/misc/a2b/src/features/Footer.js
+++ content/misc/a2b/src/features/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default function Footer() {
-  const version = process.env.REACT_APP_VERSION.replace('-alpha', 'ɑ').replace('-beta', 'β')
+  const version = process.env.REACT_APP_VERSION.replace('-alpha', 'ɑ').replace('-beta', 'β');
+  const sha = process.env.REACT_APP_GITHUB_SHA;
 
   return (
     <footer className="text-center">
       <hr />
       <p>
-        arXiv2bib v{version} • licensed under AGPLv3 • <a href="https://github.com/nidrissi/arxiv2bib">source</a>
+        arXiv2bib <span title={sha ? `Commit: ${sha}` : null}>v{version}</span> • licensed under AGPLv3 • <a href="https://github.com/nidrissi/arxiv2bib">source</a>
       </p>
     </footer>
 
