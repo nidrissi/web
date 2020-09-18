a3cb653127a18ac289269f46522ac23f1d11368a
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-09-18 15:23:56 +0200

    better looking Help page

--- content/misc/a2b/src/features/Help/index.js
+++ content/misc/a2b/src/features/Help/index.js
@@ -1,48 +1,34 @@
 import React from 'react';
 
-import faq from './faq';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-/** Table of contents based on the keys in `faq`. */
-function TOC() {
-  return (
-    <ul className="list-unstyled">
-      {faq.map(({title, key, entries }) => (
-        <li key={key}>
-          <a href={`#${key}`}>
-            <strong>
-              {title}
-            </strong>
-          </a>
-          <ul className="list-unstyled ml-2">
-            {entries.map(({key, q}) => (
-              <li key={key}>
-                <a href={`#${key}`}>
-                   {q}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
-  );
-}
+import faq from './faq';
 
 /** The FAQ entries, taken from `faq`. */
 function FAQBody() {
-  return faq.map(({ title, key, entries }) => (
-    <React.Fragment key={key}>
-      <h2 id={key}>{title}</h2>
-      <dl>
-        {entries.map(({ key, q, a }) => (
-          <React.Fragment key={key}>
-            <dt id={key}>{q}</dt>
-            <dd>{a}</dd>
-          </React.Fragment>
-        ))}
-      </dl>
-    </React.Fragment>
-  ));
+  return (
+    <>
+      {faq.map(({ title, key, entries }) => (
+        <React.Fragment key={key}>
+          <h2>{title}</h2>
+          <Accordion>
+            {entries.map(({ key, q, a }) => (
+              <Card key={key}>
+                <Accordion.Toggle as={Card.Header} className="p-2" eventKey={key}>
+                  <Button variant="link" className="p-0">{q}</Button>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={key}>
+                  <Card.Body>{a}</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            ))}
+          </Accordion>
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
 
 /** The full `Help` component. Contains a table of contents and the entries from `faq`. */
@@ -50,7 +36,6 @@ export default function Help() {
   return (
     <>
       <h1>Help</h1>
-      <TOC />
       <FAQBody />
     </>
   )
