b71e9552af4af682945add433173a7325be85dbb
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-09-18 14:07:22 +0200

    DIY: move StandardInput and StandardGroup into own file

--- content/misc/a2b/src/features/DIY/index.js
+++ content/misc/a2b/src/features/DIY/index.js
@@ -1,92 +1,16 @@
 import React from 'react';
 
-import { Formik, Form, Field, useFormikContext } from 'formik';
+import { Formik, Form, useFormikContext } from 'formik';
 
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 import BForm from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Row from 'react-bootstrap/Row';
-import Tooltip from 'react-bootstrap/Tooltip';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Entry from './Entry';
-
-/** Adds a tooltip to an input.
-    @param children The input to be tooltip.
-    @param help The help text.
- */
-function TooltipedInput({ children, help }) {
-  return (
-    <InputGroup>
-      {children}
-      <OverlayTrigger
-        placement='top'
-        overlay={
-          <Tooltip>
-            {help}
-          </Tooltip>
-        }
-      >
-        <InputGroup.Append>
-          <InputGroup.Text>
-            <FontAwesomeIcon icon="question-circle" />
-          </InputGroup.Text>
-        </InputGroup.Append>
-      </OverlayTrigger>
-    </InputGroup>
-  );
-}
-
-/** Standard inputs.
-    @param totalColumns The total number of columns for the input. Two columns will be reserved for the label.
-    @param label The text of the label
-    @param help The help text, if any.
-    @param ...props The rest of the props, passed to the input.
- */
-function StandardInput({totalColumns, label, help, ...props}) {
-  const labelColumns = 2;
-  const inputColumns = totalColumns - labelColumns;
-
-  const field = (
-    <Field
-      {...props}
-      id={props.name}
-      className='form-control'
-    />
-  );
-
-  // if there is a help text we put it in a tooltip, otherwise we keep the field
-  const input = help ? <TooltipedInput help={help} children={field} /> : field;
-
-  return (
-    <>
-      <BForm.Label
-        column sm={labelColumns}
-        htmlFor={props.name}
-      >
-        {label}
-      </BForm.Label>
-      <Col sm={inputColumns}>
-        {input}
-      </Col>
-    </>
-  )
-}
-
-/** Standard input group fields. 
-    @param props All parameters are passed to a `StandardInput`.
- */
-function StandardGroup(props) {
-  return (
-    <BForm.Group as={Row}>
-      <StandardInput totalColumns={12} {...props} />
-    </BForm.Group>
-  );
-}
+import Entry from '../Entry';
+import { StandardInput, StandardGroup } from './standard';
 
 /** Takes values from the Formik context, creates an entry and formats it with `Entry`.
  */
