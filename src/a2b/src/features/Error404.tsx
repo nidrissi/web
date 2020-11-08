4107432c283523c0db0f94a0f46be4f2f9b293de
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-08 09:48:26 +0100

    easy TS conversions

--- src/a2b/src/features/Error404.tsx
+++ src/a2b/src/features/Error404.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 /** A generic 404 error */
-export default function Err404() {
+const Error404: React.FC<{}> = () => {
   return (
     <Jumbotron>
       <h1 className="text-center">404 not found</h1>
     </Jumbotron>
   );
-}
+};
+
+export default Error404;
