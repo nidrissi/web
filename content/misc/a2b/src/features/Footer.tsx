ffcdaa19fb5d52012a53512f58eb1cc21a309db0
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-08 09:25:15 +0100

    footer.tsx

---
+++ content/misc/a2b/src/features/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/** A basic footer that contains version information, licence, and a link to the source. */
+const Footer: React.FunctionComponent<{}> = () => {
+  const version: string = process.env.REACT_APP_VERSION!.replace('-alpha', 'ɑ').replace('-beta', 'β');
+  const sha: string = process.env.REACT_APP_GITHUB_SHA || 'undefined';
+
+  return (
+    <footer className="text-center">
+      <hr />
+      <p>
+        arXiv2BibLaTeX <span title={sha ? `Commit: ${sha}` : ''}>v{version}</span> • licensed under AGPLv3 • <a href="https://github.com/nidrissi/arxiv2bib">source</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
