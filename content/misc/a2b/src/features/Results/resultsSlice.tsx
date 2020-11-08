057ffbd041110680d8dd86cd5447ce0fa7b866be
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-08 19:38:31 +0100

    EntryById was not useful

--- content/misc/a2b/src/features/Results/resultsSlice.tsx
+++ content/misc/a2b/src/features/Results/resultsSlice.tsx
@@ -78,8 +78,7 @@ export const { clearError } = resultsSlice.actions;
 
 const entriesSelectors = entriesAdapter.getSelectors((state: RootState) => state.results);
 
-export const selectEntryIds = (state: RootState) => entriesSelectors.selectIds(state);
-export const selectEntryById = (id: string | number) => (state: RootState) => entriesSelectors.selectById(state, id);
+export const selectAllEntries = (state: RootState) => entriesSelectors.selectAll(state);
 export const selectTotalEntriesFound = (state: RootState) => state.results.totalEntriesFound;
 export const selectIsLoading = (state: RootState) => state.results.isLoading;
 export const selectError = (state: RootState) => state.results.error;
