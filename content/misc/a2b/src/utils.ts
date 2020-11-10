2e5c530cda1b425bfc97ed39a42ecf866f7545e5
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-10 17:37:26 +0100

    More prettier (sic)

--- content/misc/a2b/src/utils.ts
+++ content/misc/a2b/src/utils.ts
@@ -1,7 +1,7 @@
 /** Converts accented characters into unaccented ones.
     arXiv does not allow searching for accents...
  */
-export function removeAccents(s : string) {
+export function removeAccents(s: string) {
   // https://stackoverflow.com/a/37511463
-  return s.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+  return s.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
