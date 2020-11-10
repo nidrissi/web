cdc3488f65ce95c820da5a6e4b9ae0414a7ec85a
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-10 16:22:31 +0100

    Move SubmitAndClearButtons to new file

--- src/a2b/src/features/SearchForm/index.tsx
+++ src/a2b/src/features/SearchForm/index.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Form } from 'formik';
 
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import BForm from 'react-bootstrap/Form';
-import Spinner from 'react-bootstrap/Spinner';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { setQuery } from './searchFormSlice';
 import { selectIsLoading } from '../Results/resultsSlice';
 import { InputField } from './InputField';
+import { SubmitAndClearButtons } from './SubmitAndClearButtons';
 
 /** Splits a string and removes empty entries.
     @param str The string to be split.
@@ -20,36 +14,6 @@ import { InputField } from './InputField';
 function splitter(str: string, rx: RegExp) {
   return str.split(rx).filter(s => s !== '');
 }
-
-/** The submit and clear buttons used in SearchForm.
-    @param isLoading Whether the form is currently loading or not.
- */
-const SubmitClearButtons: React.FC<{isLoading: boolean}> = ({ isLoading }) => {
-  return (
-    <BForm.Group as={BForm.Row}>
-      <Col sm={10}>
-        <Button
-          disabled={isLoading}
-          type="submit"
-          block
-        >
-          {isLoading ?
-            <span><Spinner animation="border" size="sm" /> Loading...</span>
-            : <span><FontAwesomeIcon icon="search" /> Search</span>}
-        </Button>
-      </Col>
-      <Col>
-        <Button
-          type="reset"
-          variant="secondary"
-          block
-        >
-          <FontAwesomeIcon icon="trash-alt" /> Clear
-        </Button>
-      </Col>
-    </BForm.Group>
-  );
-};
 
 /** The full search form. It has
     - three fields:
@@ -111,7 +75,7 @@ const SearchForm: React.FC<{}> = () => {
           placeholder="operad & configuration space & ..."
           title="Words/sentences to search in the title separated by '&'."
         />
-        <SubmitClearButtons isLoading={isLoading} />
+        <SubmitAndClearButtons isLoading={isLoading} />
       </Form>
     </Formik>
   );
