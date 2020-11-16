f7d8afae61d89eae680cfadb172d9c40b6da1eaf
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-11-16 19:23:10 +0100

    New GA

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,4 +1,5 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-170792065-2');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S549JC61XZ');
