54d32c90ee61e34c555533509031aaf4304436bb
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-06 13:35:37 +0200

    fix toast

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
+window.onload = function () {
   if (
     !document.cookie
       .split("; ")
       .find((row) => row.startsWith("cookieToastShown"))
   ) {
-    $("#cookie-toast").toast("show");
+    var toastElement = document.getElementById("cookie-toast");
+    var toast = new bootstrap.Toast(toastElement);
+    toast.show();
     document.cookie =
       "cookieToastShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
   }
-});
+};
 
 window.dataLayer = window.dataLayer || [];
 function gtag() {
