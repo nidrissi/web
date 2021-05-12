4e3d6a9e4b5f66769221d37ffceddb6870bb500c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-12 17:02:39 +0200

    md styles

--- src/a2b/tailwind.config.js
+++ src/a2b/tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              "text-decoration": "none",
+              "font-weight": "normal",
+              "&:hover": { "text-decoration": "underline" },
+            },
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
