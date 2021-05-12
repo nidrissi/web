1c9eed51a44a86bed2abd3794072748c4d6343ee
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-12 22:52:50 +0200

    page titles

--- content/misc/a2b/src/Page.tsx
+++ content/misc/a2b/src/Page.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from "react";
 
-export const Page: React.FC<{ title?: string }> = ({ title, children }) => {
+export const Page: React.FC<{ title: string }> = ({ title, children }) => {
   useEffect(() => {
     document.title = "arXiv2BibLaTeX" + (title ? " | " + title : "");
   }, [title]);
-  return <>{children}</>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-3">{title}</h1>
+      {children}
+    </div>
+  );
 };
