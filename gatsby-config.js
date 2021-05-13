92ae58912f732a30b57a41bd3e9d9a04d3a6b577
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-13 17:03:14 +0200

    more dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/post`,
+        name: `post`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+  ],
 };
