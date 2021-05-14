ca048bc8c4d66c8ee97343345b004c50ad3093eb
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-14 14:04:53 +0200

    move global JS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,24 @@
 import "./src/styles/global.css";
 import "katex/dist/katex.css";
+exports.onClientEntry = () => {
+  window.onload = function () {
+    if (
+      !document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("cookieToastShown"))
+    ) {
+      var toastElement = document.getElementById("cookie-toast");
+      var toast = new bootstrap.Toast(toastElement);
+      toast.show();
+      document.cookie =
+        "cookieToastShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    }
+  };
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-S549JC61XZ");
+};
