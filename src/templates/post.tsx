510d599c42c869e1ec4e8325aa3e16c08f2b955e
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-14 21:42:50 +0200

    Add links to posts

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -3,12 +3,12 @@ import { graphql } from "gatsby";
 import React from "react";
 
 import Layout from "../components/Layout";
-import Meta from "../components/meta/data";
+import MetaData from "../components/meta/data";
 
 const PostTemplate: React.FC<{ data: PostTemplateQuery }> = ({ data }) => {
   const {
     body,
-    frontmatter: { title, date, lastMod, tags },
+    frontmatter: { title, date, lastMod, tags, urls },
   } = data.mdx;
 
   return (
@@ -16,10 +16,11 @@ const PostTemplate: React.FC<{ data: PostTemplateQuery }> = ({ data }) => {
       <header className="mb-3">
         <h1 className="mb-0 text-3xl font-bold text-gray-700">{title}</h1>
         <p>
-          <Meta
+          <MetaData
             date={new Date(date)}
             lastMod={lastMod ? new Date(lastMod) : null}
             tags={tags}
+            urls={urls}
           />
         </p>
       </header>
@@ -39,6 +40,9 @@ type PostTemplateQuery = {
       date: string;
       lastMod?: string;
       tags: string[];
+      urls: {
+        source: string;
+      };
     };
   };
 };
@@ -53,6 +57,9 @@ export const query = graphql`
         date
         lastMod
         tags
+        urls {
+          source
+        }
       }
     }
   }
