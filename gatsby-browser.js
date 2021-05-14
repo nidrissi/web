58ef9d5256bb60b684718020f8e9104821b82dba
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-14 22:24:35 +0200

    fix big FA icons on start
    https://stackoverflow.com/a/59429852/15543974

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,3 @@
-exports.onInitialClientRender = () => {
-  // window.onload = function () {
-  //   if (
-  //     !document.cookie
-  //       .split("; ")
-  //       .find((row) => row.startsWith("cookieToastShown"))
-  //   ) {
-  //     var toastElement = document.getElementById("cookie-toast");
-  //     var toast = new bootstrap.Toast(toastElement);
-  //     toast.show();
-  //     document.cookie =
-  //       "cookieToastShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  //   }
-  // };
-};
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
