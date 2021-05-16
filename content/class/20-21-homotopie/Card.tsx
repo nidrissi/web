1a8fd85ae4f7e9e5130663d473e6cac7d8c4ec88
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-16 21:22:10 +0200

    span.block === div...

--- content/class/20-21-homotopie/Card.tsx
+++ content/class/20-21-homotopie/Card.tsx
@@ -12,12 +12,12 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({ number, date, children, blackboard, video }) => {
   return (
     <div className="flex flex-col border border-gray-400 rounded-md gap-2">
-      <span className="block p-1 bg-gray-200 rounded-t-md">
+      <div className="p-1 bg-gray-200 rounded-t-md">
         <span className="font-semibold text-lg">Lecture {number}</span>
         {' · '}
         {date}
-      </span>
-      <span className="block flex-grow p-1">{children}</span>
+      </div>
+      <div className="flex-grow p-1">{children}</div>
       <div className="flex flex-col gap-2 sm:flex-row sm:divide-x divide-gray-500 text-center bg-gray-100 rounded-b-md p-1">
         <a href={blackboard} className="block flex-grow"><FontAwesomeIcon icon={faChalkboard} />&nbsp;Blackboard</a>
         <a href={video} className="block flex-grow"><FontAwesomeIcon icon={faPlayCircle} />&nbsp;Video</a>
