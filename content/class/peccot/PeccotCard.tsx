8ef5db2d13bd9d908c596feb574933f6cdf2bc75
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-17 13:43:09 +0200

    regular => solid

--- content/class/peccot/PeccotCard.tsx
+++ content/class/peccot/PeccotCard.tsx
@@ -1,4 +1,4 @@
-import { faPlayCircle } from "@fortawesome/free-regular-svg-icons"
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 
