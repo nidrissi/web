314f75c63c0ebaf1e4046c78c31373fbd0748980
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-17 14:20:12 +0200

    fix PeccotCard height

--- content/class/peccot/PeccotCard.tsx
+++ content/class/peccot/PeccotCard.tsx
@@ -11,19 +11,21 @@ type PeccotCardProps = {
 
 const PeccotCard: React.FC<PeccotCardProps> = ({ link, number, date, children }) => {
   return (
-    <div className="border rounded-md flex flex-col gap-1">
+    <div className="border rounded-md flex flex-col gap-1 h-full">
+      {/* Awful hack. tailwind-prose puts a margin on images and I couldn't find how to disable it. */}
+      <div className="-mb-9"></div>
       <a
         className="block"
         href={link}
       >
         <img src={`/img/peccot/thumb${number}.jpg`} alt={`Photo of the lecture ${number}`} className="w-full rounded-t-md" />
       </a>
-      <div>
+      <div className="-mt-3">
         <span className="font-bold text-xl">Lesson {number}</span>
         <br />
         <span className="text-gray-700 font-semibold">{date}</span>
       </div>
-      <div>
+      <div className="flex-grow">
         {children}
       </div>
       <a className="block p-2 bg-gray-200 rounded-b-md text-center" href={link}>
