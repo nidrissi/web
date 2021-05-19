ea0ff99437da42eff5c6abafafcd587232788b96
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-19 09:52:32 +0200

    smaller icon

--- src/components/Root/SeeMore.tsx
+++ src/components/Root/SeeMore.tsx
@@ -9,7 +9,7 @@ type SeeMoreProps = {
 }
 const SeeMore: React.FC<SeeMoreProps> = ({ to, label }) => (
   <Link to={to} className={`block text-center text-green-800 border border-green-700 hover:text-white hover:bg-green-700 py-1 rounded-md`}>
-    See more {label} <FontAwesomeIcon icon={faArrowRight} />
+    See more {label} <FontAwesomeIcon icon={faArrowRight} size="sm" />
   </Link>
 );
 export default SeeMore;
