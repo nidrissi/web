0be0269ffc37cf68a3adc2ac83bc551fc8511d2e
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-19 19:15:14 +0200

    spinner

--- src/a2b/features/SearchForm/SubmitAndClearButtons.tsx
+++ src/a2b/features/SearchForm/SubmitAndClearButtons.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faSpinner, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 /** The submit and clear buttons used in SearchForm.
     @param isLoading Whether the form is currently loading or not.
@@ -11,18 +11,21 @@ const SubmitAndClearButtons: React.FC<{ isLoading: boolean }> = ({
   return (
     <div className="row-span-full flex space-x-2">
       <button
-        className={`block flex-grow p-2 bg-blue-800 text-white rounded-md ${
-          isLoading ? "cursor-not-allowed" : ""
-        }`}
+        className={`block flex-grow p-2 bg-blue-800 text-white rounded-md ${isLoading ? "cursor-not-allowed" : ""
+          }`}
         disabled={isLoading}
         type="submit"
       >
         {isLoading ? (
-          <span>Loading...</span>
+          <>
+            <FontAwesomeIcon icon={faSpinner} className="mr-1" spin />
+            Loading..
+          .</>
         ) : (
-          <span>
-            <FontAwesomeIcon icon={faSearch} /> Search
-          </span>
+          <>
+            <FontAwesomeIcon icon={faSearch} className="mr-1" />
+            Search
+          </>
         )}
       </button>
       <button
