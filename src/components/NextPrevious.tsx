3196ac0bddeea2fc6f01ccd113b5446d67feffe8
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-29 09:37:18 +0200

    extract nextprevious

---
+++ src/components/NextPrevious.tsx
@@ -0,0 +1,51 @@
+import { Link } from "gatsby";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { actualTitle } from "./Page";
+
+export type NextPreviousProps = {
+  slug: string;
+  frontmatter: {
+    title: string;
+    event: string;
+    location: string;
+    year: string;
+  };
+}
+
+export const NextPrevious: React.FC<{ previous: NextPreviousProps; next: NextPreviousProps; type: string; }> = ({
+  next, previous, type
+}) => {
+  if (!next && !previous) {
+    return null;
+  }
+  if (type === 'misc' || type === 'class') {
+    return null;
+  }
+
+  const linkStyle = "block p-1 text-sm text-green-700 border border-green-700 rounded-md hover:bg-green-700 hover:text-white";
+
+  return (
+    <div className="flex justify-between w-full mt-6">
+      {previous ? (
+        <Link
+          to={`/${type}/${previous.slug}`}
+          className={linkStyle}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} className="mr-1" />
+          {actualTitle(previous.frontmatter, type)}
+        </Link>
+      ) : <div></div>}
+      {next ? (
+        <Link
+          to={`/${type}/${next.slug}`}
+          className={linkStyle}
+        >
+          {actualTitle(next.frontmatter, type)}
+          <FontAwesomeIcon icon={faArrowRight} className="ml-1" />
+        </Link>
+      ) : <div></div>}
+    </div>
+  );
+};
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,22 +1,11 @@
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import React from "react";
 
 import Layout from "./Layout";
 import Meta, { Frontmatter } from "./meta";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import TOC, { TableOfContents } from "./TOC";
-
-type NextPreviousProps = {
-  slug: string;
-  frontmatter: {
-    title: string;
-    event: string;
-    location: string;
-    year: string;
-  };
-}
+import { NextPrevious, NextPreviousProps } from "./NextPrevious";
 
 type PageTemplateProps = {
   data: {
@@ -49,43 +38,6 @@ export function actualTitle(
     `${frontmatter.title} (${frontmatter.year})`
   ) : (
     frontmatter.title
-  );
-}
-
-const NextPrevious: React.FC<{ previous: NextPreviousProps, next: NextPreviousProps, type: string }> = ({
-  next, previous, type
-}) => {
-  if (!next && !previous) {
-    return null;
-  }
-  if (type === 'misc' || type === 'class') {
-    return null;
-  }
-
-  const linkStyle = "block p-1 text-sm text-green-700 border border-green-700 rounded-md hover:bg-green-700 hover:text-white";
-
-  return (
-    <div className="flex justify-between w-full mt-6">
-      {previous ? (
-        <Link
-          to={`/${type}/${previous.slug}`}
-          className={linkStyle}
-        >
-          <FontAwesomeIcon icon={faArrowLeft} className="mr-1" />
-          {actualTitle(previous.frontmatter, type)}
-        </Link>
-      ) : <div></div>}
-      {next ? (
-        <Link
-          to={`/${type}/${next.slug}`}
-          className={linkStyle}
-        >
-          {actualTitle(next.frontmatter, type)}
-          <FontAwesomeIcon icon={faArrowRight} className="ml-1" />
-        </Link>
-      ) : <div></div>}
-    </div>
-
   );
 }
 
