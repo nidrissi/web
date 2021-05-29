acadf4317a9a8f195ecadb2d423f8fe4d14d31cd
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-29 12:04:46 +0200

    restyle TOC

--- src/components/TOC.tsx
+++ src/components/TOC.tsx
@@ -14,16 +14,21 @@ const TOC: React.FC<{ toc: TableOfContents }> = ({ toc: { items } }) => {
   const [show, setShow] = useState(false);
 
   return items ? (
-    <div className="float-right max-w-lg p-2 m-2 rounded-md bg-blue-800 text-white">
-      <button className="block focus:outline-none w-full" onClick={() => setShow(!show)}>
+    <div className="max-w-md my-2 rounded-md border border-blue-800 text-blue-800">
+      <button
+        className="block focus:outline-none w-full p-1 text-lg hover:bg-blue-800 hover:text-white"
+        onClick={() => setShow(!show)}
+      >
         <FontAwesomeIcon icon={faParagraph} className="mr-1" />
         Contents
         <FontAwesomeIcon icon={faCaretDown} className="ml-1" />
       </button>
-      <ol className={`${show ? 'block' : 'hidden'} border-t border-white text-lg list-decimal mt-2 mb-1`}>
+      <ol className={`${show ? 'block' : 'hidden'} list-decimal list-inside border-t border-white mt-2 mb-1 px-1`}>
         {items.map(({ url, title }) => (
-          <li key={url} className="mx-4">
-            <Link to={url}>{title}</Link>
+          <li key={url}>
+            <Link to={url} className="hover:underline">
+              {title}
+            </Link>
           </li>
         ))}
       </ol>
