b42032b2df96a6a198cc2259728e673e1bbd79ec
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-05-31 13:15:50 +0200

    ldjson => children

--- src/components/meta/data.tsx
+++ src/components/meta/data.tsx
@@ -8,17 +8,17 @@ import TagLink from "./TagLink";
 const MetaData: React.FC<{ frontmatter: Frontmatter }> = ({
   frontmatter: { title, date, lastMod, tags }
 }) => {
-  const data = {
-    "@context": "https://schema.org",
-    "@type": "Article",
-    "headline": title,
-    "datePublished": new Date(date).toISOString(),
-    "dateModified": lastMod ? new Date(lastMod).toISOString() : null,
-  };
-
   return (
     <>
-      <LdJSON data={data} />
+      <LdJSON>
+        {{
+          "@context": "https://schema.org",
+          "@type": "Article",
+          "headline": title,
+          "datePublished": new Date(date).toISOString(),
+          "dateModified": lastMod ? new Date(lastMod).toISOString() : null,
+        }}
+      </LdJSON>
       <DateTime date={date} label="Published" />
       <DateTime date={lastMod} label="Updated" />
       {tags?.sort().map((tag) => (
