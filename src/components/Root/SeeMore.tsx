d9cba90990374a91cc9e4113a1af417d1e8cf4e5
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-06-03 16:48:00 +0200

    shadows

--- src/components/Root/SeeMore.tsx
+++ src/components/Root/SeeMore.tsx
@@ -6,7 +6,7 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 const SeeMore: React.FC<{ to: string }> = ({ to, children }) => (
   <Link
     to={to}
-    className="block text-center text-green-800 border border-green-700 hover:text-white hover:bg-green-700 py-1 rounded-md"
+    className="block text-center text-green-800 border border-green-700 hover:text-white hover:bg-green-700 hover:shadow-md py-1 rounded-md"
   >
     See more {children}&nbsp;<FontAwesomeIcon icon={faChevronRight} size="sm" />
   </Link>
