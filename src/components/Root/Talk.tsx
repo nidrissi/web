deb4fb4e3f70c27bf230bb09eace315df0bafd88
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-06-07 17:21:49 +0200

    type

--- src/components/Root/Talk.tsx
+++ src/components/Root/Talk.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Mini from "../Mini";
 import SeeMore from "./SeeMore";
+import { Frontmatter } from "../meta";
+
+type RootTalkQuery = {
+  allMdx: {
+    nodes: {
+      slug: string;
+      wordCount: { words: Number; };
+      frontmatter: Frontmatter;
+    }[];
+  }
+};
 
 const Talk: React.FC<{}> = () => {
-  const { allMdx: { nodes } } = useStaticQuery(graphql`
+  const { allMdx: { nodes } }: RootTalkQuery = useStaticQuery(graphql`
 query RootTalkQuery {
   allMdx(
     filter: {fields: {type: {eq: "talk"}}}
@@ -30,16 +41,13 @@ query RootTalkQuery {
   }
 }`);
 
-
   return (
     <section>
       <h2 className="text-4xl font-bold mb-3">Talks</h2>
       <div className="flex flex-col gap-4">
-        {
-          nodes.map(({ frontmatter, slug, wordCount: { words } }) => (
-            <Mini key={slug} type="talk" slug={slug} frontmatter={frontmatter} noLink={words === 0} />
-          ))
-        }
+        {nodes.map(({ frontmatter, slug, wordCount: { words } }) => (
+          <Mini key={slug} type="talk" slug={slug} frontmatter={frontmatter} noLink={words === 0} />
+        ))}
         <SeeMore to="/talk">talks</SeeMore>
       </div>
     </section>
