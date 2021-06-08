dddf02562c20bef1d512cd61c2eb44ff74c38531
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-06-08 19:14:34 +0200

    fix gaps

--- src/components/Root/Research.tsx
+++ src/components/Root/Research.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
 import Mini from "../Mini";
 import SeeMore from "./SeeMore";
+import { Frontmatter } from "../meta";
+
+type RootResearchQuery = {
+  allMdx: {
+    nodes: {
+      slug: string;
+      frontmatter: Frontmatter;
+    }[];
+  }
+};
 
 const Research: React.FC<{}> = () => {
-  const { allMdx: { nodes } } = useStaticQuery(graphql`
+  const { allMdx: { nodes } }: RootResearchQuery = useStaticQuery(graphql`
 query RootResearchQuery {
   allMdx(
     filter: {fields: {type: {eq: "research"}}, frontmatter: {status: {regex: "/publication|preprint/"}}}
@@ -31,13 +41,11 @@ query RootResearchQuery {
     <section>
       <h2 className="text-4xl font-bold mb-3">Research</h2>
       <div className="flex flex-col gap-4">
-        {
-          nodes.map(({ frontmatter, slug }) => {
-            return (
-              <Mini key={slug} type="research" slug={slug} frontmatter={frontmatter} />
-            );
-          })
-        }
+        {nodes.map(({ frontmatter, slug }) => {
+          return (
+            <Mini key={slug} type="research" slug={slug} frontmatter={frontmatter} />
+          );
+        })}
         <SeeMore to="/research">research</SeeMore>
       </div>
     </section>
